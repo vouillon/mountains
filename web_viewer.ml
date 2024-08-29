c2018cdca0d3debb5286119fdb33701c8b1feea5
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-08-30 01:14:01 +0200

    -

--- web_viewer.ml
+++ web_viewer.ml
@@ -1,3 +1,38 @@
+(*
+function toReadableStream(value) {
+	return new ReadableStream({
+		start(controller) {
+			controller.enqueue(value);
+			controller.close();
+		},
+	});
+}
+var s = toReadableStream(new Uint16Array([1,2,3])).pipeThrough(new DecompressionStream('deflate-raw'))
+function concatArrayBuffers(chunks) {
+    const result = new Uint8Array(chunks.reduce((a, c) => a + c.length, 0));
+    let offset = 0;
+    for (const chunk of chunks) {
+        result.set(chunk, offset);
+        offset += chunk.length;
+    }
+    return result;
+}
+export async function streamToArrayBuffer(stream) {
+    const chunks = [];
+    const reader = stream.getReader();
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        } else {
+            chunks.push(value);
+        }
+    }
+    return concatArrayBuffers(chunks);
+}
+streamToArrayBuffer(s)
+*)
+
 let ( let* ) = Result.bind
 let pi = 4. *. atan 1.
 
