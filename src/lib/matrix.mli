4ed9634d04fe81a7cf4ebd50d2dfeb21d27b9dd1
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-12-27 18:16:59 +0100

    WIP

--- src/lib/matrix.mli
+++ src/lib/matrix.mli
@@ -1,4 +1,4 @@
-type t
+type t = float array
 type vector = { x : float; y : float; z : float; w : float }
 
 val ( * ) : t -> t -> t
@@ -10,6 +10,18 @@ val rotate_x : float -> t
 val rotate_y : float -> t
 val rotate_z : float -> t
 val project : x_scale:float -> y_scale:float -> near_plane:float -> t
+
+val ortho :
+  left:float ->
+  right:float ->
+  bottom:float ->
+  top:float ->
+  near:float ->
+  far:float ->
+  t
+
+val look_at : eye:vector -> center:vector -> up:vector -> t
+val inverse : t -> t
 
 val array :
   t -> (float, Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array1.t
