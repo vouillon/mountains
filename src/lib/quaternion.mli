4ed7103c670a862a21dced5c68d0bc238b633a23
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-01-21 21:37:52 +0100

    -

---
+++ src/lib/quaternion.mli
@@ -0,0 +1,11 @@
+type t = { x : float; y : float; z : float; w : float }
+
+val create : float -> float -> float -> float -> t
+val identity : t
+val normalize : t -> t
+val mult : t -> t -> t
+val ( * ) : t -> t -> t
+val conjugate : t -> t
+val from_axis_angle : Matrix.vector -> float -> t
+val to_matrix : t -> Matrix.t
+val to_euler : t -> float * float * float
