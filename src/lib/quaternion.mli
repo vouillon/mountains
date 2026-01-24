bfac23aa79e5203bb20a4e2c60d45ae1ee8629b3
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-01-24 14:26:36 +0100

    -

--- src/lib/quaternion.mli
+++ src/lib/quaternion.mli
@@ -11,4 +11,5 @@ val slerp : t -> t -> float -> t
 val from_axis_angle : Matrix.vector -> float -> t
 val transform_vector : t -> Matrix.vector -> Matrix.vector
 val to_matrix : t -> Matrix.t
+val angle_between : t -> t -> float
 val to_euler : t -> float * float * float
